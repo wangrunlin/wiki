bdb91961f4299d2abc0b1576c20ba675cc022344
Leo <leo@wangrunlin.com>
2022-05-05 18:08:31 +0800

    Update deploy

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,16 +1,15 @@
 module.exports = {
-	base: '/wiki/',
-	dest: './docs',
-	title: 'Leo\'s Personal wiki',
-	description: '王润林的个人维基 - Leo\'s Personal Wiki',
+	base: "/wiki/",
+	title: "Leo's Personal wiki",
+	description: "王润林的个人维基 - Leo's Personal Wiki",
 	themeConfig: {
-	sidebar: [
-		['/', 'README'],
-		['/cs-study-way/', 'CS 学习路线'],
-		['/tools/', '工具推荐'],
-		['/install-config/', '安装配置'],
-		['/exception/', '报错处理'],
-		['/about', '关于']
-		]
-	}
-}
+		sidebar: [
+			["/", "README"],
+			["/cs-study-way/", "CS 学习路线"],
+			["/tools/", "工具推荐"],
+			["/install-config/", "安装配置"],
+			["/exception/", "报错处理"],
+			["/about", "关于"],
+		],
+	},
+};
